5267d16b18c689c484ddb9b680e7bdc7ce9a1ae6
yonatanabera <45354198+yonatanabera@users.noreply.github.com>
2021-07-08 11:19:38 +0300

    Update ZipController.php

--- ZipController.php
+++ ZipController.php
@@ -13,37 +13,60 @@ class HomeController extends Controller
 
     public function download_zip(){
         $zip = new ZipArchive;
-
-   
-
         $fileName = 'myNewFile.zip';
-
-   
-
         if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE)
-
         {
-
             $files = File::files(public_path('temp/'));
-
-   
-
             foreach ($files as $key => $value) {
-
                 $relativeNameInZipFile = basename($value);
-
                 $zip->addFile($value, $relativeNameInZipFile);
-
             }
-
-             
-
             $zip->close();
-
         }
+        return response()->download(public_path($fileName));
+    }
+
+
+    public function create()
+    {
+        $filePath = 'app/course/files.zip';
+        $zip = new \ZipArchive();
+    
+        if ($zip->open($filePath, \ZipArchive::CREATE) !== true) {
+            throw new \RuntimeException('Cannot open ' . $filePath);
+        }
+    
+        $this->addContent($zip, realpath('app/course'));
+        $zip->close();
+    }
 
     
-
-        return response()->download(public_path($fileName));
+   
+    private function addContent(\ZipArchive $zip, string $path)
+    {
+      
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+                $path,
+                \FilesystemIterator::FOLLOW_SYMLINKS
+            ),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+    
+        while ($iterator->valid()) {
+            if (!$iterator->isDot()) {
+                $filePath = $iterator->getPathName();
+                $relativePath = substr($filePath, strlen($path) + 1);
+    
+                if (!$iterator->isDir()) {
+                    $zip->addFile($filePath, $relativePath);
+                } else {
+                    if ($relativePath !== false) {
+                        $zip->addEmptyDir($relativePath);
+                    }
+                }
+            }
+            $iterator->next();
+        }
     }
 }
